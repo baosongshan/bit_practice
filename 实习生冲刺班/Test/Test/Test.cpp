a7f03b515a4290f2736495577c0a163103c44149
baosongshan <baosongshan2006@163.com>
2020-07-25 12:30:24 +0800

    add oj

--- 实习生冲刺班/Test/Test/Test.cpp
+++ 实习生冲刺班/Test/Test/Test.cpp
@@ -1,8 +1,308 @@
+#include<iostream>
 #include<stdio.h>
 #include<stdlib.h>
 #include<assert.h>
 #include<vld.h>
+#include<list>
+#include<stack>
+using namespace std;
 
+void main()
+{
+	struct ListNode* middleNode(struct ListNode* head)
+	{
+		int x = 0;
+		struct ListNode *p = head;
+		while (p != NULL)
+		{
+			p = p->next;
+			x++;
+		}
+		if (x % 2 == 0)
+		{
+			x = x / 2 + x % 2;
+		}
+		else
+		{
+			x = x / 2;
+		}
+		p = head;
+		while (x-- > 0)
+		{
+			p = p->next;
+		}
+		return p;
+	}
+}
+
+/*
+typedef char ElemType;
+typedef struct LinkStackNode
+{
+	ElemType data;
+	struct LinkStackNode *link;
+}LinkStackNode;
+
+typedef LinkStackNode* LinkStack;
+void LinkStackInit(LinkStack *pst);
+void LinkStackPush(LinkStack *pst, ElemType x);
+void LinkStackPop(LinkStack *pst);
+ElemType LinkStackTop(LinkStack *pst);
+int LinkStackSize(LinkStack *pst);
+bool LinkStackEmpty(LinkStack *pst);
+void LinkStackDestroy(LinkStack *pst);
+/////////////////////////////////////////////////////
+
+void LinkStackInit(LinkStack *pst)
+{
+	assert(pst != NULL);
+	*pst = NULL;
+}
+
+void LinkStackPush(LinkStack *pst, ElemType x)
+{
+	assert(pst != NULL);
+	LinkStackNode *node = (LinkStackNode*)malloc(sizeof(LinkStackNode));
+	assert(node != NULL);
+	node->data = x;
+
+	node->link = *pst;
+	*pst = node;
+}
+void LinkStackPop(LinkStack *pst)
+{
+	assert(pst != NULL);
+	if(*pst != NULL)
+	{
+		LinkStackNode *p = *pst;
+		*pst = p->link;
+		free(p);
+	}
+}
+ElemType LinkStackTop(LinkStack *pst)
+{
+	assert(pst != NULL && *pst != NULL);
+	return (*pst)->data;
+}
+
+int LinkStackSize(LinkStack *pst)
+{
+	assert(pst != NULL);
+	int size = 0;
+	LinkStackNode *p = *pst;
+	while(p != NULL)
+	{
+		size++;
+		p = p->link;
+	}
+	return size;
+}
+
+bool LinkStackEmpty(LinkStack *pst)
+{
+    assert(pst != NULL);
+    return *pst == NULL;
+}
+
+void LinkStackDestroy(LinkStack *pst)
+{
+	assert(pst != NULL);
+	LinkStackNode *p = *pst;
+	while(p != NULL)
+	{
+		*pst = p->link;
+		free(p);
+		p = *pst;
+	}
+}
+
+//////////////////////////////////////////////////////////
+
+typedef struct {
+    LinkStack st;
+    LinkStack min_st;
+} MinStack;
+
+/** initialize your data structure here. */
+
+MinStack* minStackCreate() {
+    MinStack *pst = (MinStack*)malloc(sizeof(MinStack));
+    LinkStackInit(&(pst->st));
+    LinkStackInit(&(pst->min_st));
+    return pst;
+}
+
+void minStackPush(MinStack* obj, int x) {
+    LinkStackPush(&(obj->st), x);
+    if(LinkStackEmpty(&(obj->min_st)) || x<=LinkStackTop(&(obj->min_st)))
+        LinkStackPush(&(obj->min_st), x);
+}
+
+void minStackPop(MinStack* obj) {
+    ElemType topval = LinkStackTop(&(obj->st));
+    LinkStackPop(&(obj->st));
+    if(topval == LinkStackTop(&(obj->min_st)))
+        LinkStackPop(&(obj->min_st));
+}
+
+int minStackTop(MinStack* obj) {
+    return LinkStackTop(&(obj->st));
+}
+
+int minStackGetMin(MinStack* obj) {
+    return LinkStackTop(&(obj->min_st));
+}
+
+void minStackFree(MinStack* obj) {
+    LinkStackDestroy(&(obj->st));
+    LinkStackDestroy(&(obj->min_st));
+    free(obj);
+}
+
+void main()
+{
+
+}
+
+/*
+void main()
+{
+	list<int> mylist;
+	for(int i=1; i<=5; ++i)
+		mylist.push_back(i);
+
+
+}
+
+/*
+typedef char ElemType;
+typedef struct LinkStackNode
+{
+	ElemType data;
+	struct LinkStackNode *link;
+}LinkStackNode;
+
+typedef LinkStackNode* LinkStack;
+void LinkStackInit(LinkStack *pst);
+void LinkStackPush(LinkStack *pst, ElemType x);
+void LinkStackPop(LinkStack *pst);
+ElemType LinkStackTop(LinkStack *pst);
+int LinkStackSize(LinkStack *pst);
+bool LinkStackEmpty(LinkStack *pst);
+void LinkStackDestroy(LinkStack *pst);
+/////////////////////////////////////////////////////
+
+void LinkStackInit(LinkStack *pst)
+{
+	assert(pst != NULL);
+	*pst = NULL;
+}
+
+void LinkStackPush(LinkStack *pst, ElemType x)
+{
+	assert(pst != NULL);
+	LinkStackNode *node = (LinkStackNode*)malloc(sizeof(LinkStackNode));
+	assert(node != NULL);
+	node->data = x;
+
+	node->link = *pst;
+	*pst = node;
+}
+void LinkStackPop(LinkStack *pst)
+{
+	assert(pst != NULL);
+	if(*pst != NULL)
+	{
+		LinkStackNode *p = *pst;
+		*pst = p->link;
+		free(p);
+	}
+}
+ElemType LinkStackTop(LinkStack *pst)
+{
+	assert(pst != NULL && *pst != NULL);
+	return (*pst)->data;
+}
+
+int LinkStackSize(LinkStack *pst)
+{
+	assert(pst != NULL);
+	int size = 0;
+	LinkStackNode *p = *pst;
+	while(p != NULL)
+	{
+		size++;
+		p = p->link;
+	}
+	return size;
+}
+
+bool LinkStackEmpty(LinkStack *pst)
+{
+    assert(pst != NULL);
+    return *pst == NULL;
+}
+
+void LinkStackDestroy(LinkStack *pst)
+{
+	assert(pst != NULL);
+	LinkStackNode *p = *pst;
+	while(p != NULL)
+	{
+		*pst = p->link;
+		free(p);
+		p = *pst;
+	}
+}
+//////////////////////////////////////////////////////////////
+
+bool isValid(char * s)
+{
+    if(s == NULL)
+        return false;
+    if(*s == '\0')
+        return true;
+
+    LinkStack st;
+    LinkStackInit(&st);
+
+    while(*s != '\0')
+    {
+        if(*s=='{' || *s=='[' || *s=='(')
+            LinkStackPush(&st, *s);
+        else
+        {
+			if(LinkStackEmpty(&st))
+				return false;
+            char top = LinkStackTop(&st);
+            if((*s=='}'&&top!='{') || (*s==']'&&top!='[') || (*s==')'&&top!='('))
+                return false;
+            LinkStackPop(&st);
+        }
+        s++;
+    }
+    bool flag = LinkStackEmpty(&st);
+    LinkStackDestroy(&st);
+    return flag;
+}
+
+void main()
+{
+	char *str = "[()]";
+	bool flag = isValid(str);
+	if(flag)
+		printf("OK.\n");
+	else
+		printf("Error.\n");
+}
+
+
+
+
+
+
+
+/*
 void SeqListEraseAll(int *ar, int *n, int key)
 {
 
