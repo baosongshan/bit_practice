71e050d924e8ca744d50985f075ff5e8858abd19
baosongshan <baosongshan2006@163.com>
2020-08-03 12:32:32 +0800

    add bst

--- 实习生冲刺班/DS/DataStruct/DataStruct/TestMain.cpp
+++ 实习生冲刺班/DS/DataStruct/DataStruct/TestMain.cpp
@@ -5,9 +5,68 @@
 //#include"stack.h"
 //#include"queue.h"
 //#include"bintree.h"
+//#include"heap.h"
 
-#include"heap.h"
+#include"bst.h"
 
+void main()
+{
+	int ar[] = {27,15,19,18,1,28,34,65,100,49,25,37};
+	int n = sizeof(ar) / sizeof(ar[0]);
+
+	BSTree bst;
+	BstInit(&bst);
+
+	for(int i=0; i<n; ++i)
+		BstInsert(&bst, ar[i]);
+
+	printf("min = %d\n", BstMin(bst));
+	printf("max = %d\n", BstMax(bst));
+
+	BstSortPrint(bst);
+	printf("\n");
+
+	BSTNode *p = BstFind(bst, 37);
+	BstErase(&bst, p);
+
+}
+
+
+/*
+void main()
+{
+	//int ar[] = {27,15,19,18,28,34,65,49,25,37};
+	int ar[] = {53, 17, 78, 9, 45, 65, 87, 23};
+	int n = sizeof(ar) / sizeof(ar[0]);
+	MinHeap mhp;
+	HeapSort(&mhp, ar, n);
+
+	//HeapShow(&mhp);
+}
+
+/*
+void main()
+{
+	int v = (0-1)/2;
+	int ar[] = {27,15,19,18,28,34,65,49,25,37};
+	int n = sizeof(ar) / sizeof(ar[0]);
+
+	MinHeap mhp;
+	HeapInit(&mhp);
+
+	for(int i=0; i<n; ++i)
+		HeapInsert(&mhp, ar[i]);
+
+	HeapShow(&mhp);
+
+	HeapInsert(&mhp, 17);
+	HeapShow(&mhp);
+
+	HeapErase(&mhp);
+	HeapShow(&mhp);
+}
+
+/*
 void main()
 {
 	int ar[] = {27,15,19,18,28,34,65,49,25,37};
--- 实习生冲刺班/DS/DataStruct/DataStruct/heap.h
+++ 实习生冲刺班/DS/DataStruct/DataStruct/heap.h
@@ -14,11 +14,15 @@ typedef struct MinHeap
 }MinHeap;
 
 void HeapInit(MinHeap *php,  int sz = HEAP_DEFAULT_SIZE);
+void HeapCreate(MinHeap *php, int ar[], int n);
 void HeapInsert(MinHeap *php, HeapElemType x);
 void HeapErase(MinHeap *php);
 void HeapShow(MinHeap *php);
+bool HeapEmpty(MinHeap *php);
+HeapElemType HeapTop(MinHeap *php);
+void HeapSort(MinHeap *php, int ar[], int n);
 void HeapShiftUp(MinHeap *php, int start);
-void HeapShiftDown(MinHeap *php, int start);
+void HeapShiftDown(MinHeap *php, int start, int n);
 /////////////////////////////////////////////////////////////////////
 void HeapInit(MinHeap *php, int sz)
 {
@@ -27,6 +31,24 @@ void HeapInit(MinHeap *php, int sz)
 	php->base = (HeapElemType*)malloc(sizeof(HeapElemType) * php->capacity);
 	memset(php->base, 0, sizeof(HeapElemType) * php->capacity);
 	php->size = 0;
+}
+
+void HeapCreate(MinHeap *php, HeapElemType ar[], int n)
+{
+	php->capacity = n;
+	php->base = (HeapElemType*)malloc(sizeof(HeapElemType) * php->capacity);
+	assert(php->base != NULL);
+	for(int i=0; i<n; ++i)
+		php->base[i] = ar[i];
+	php->size = n;
+
+	//堆调整
+	int curpos = n/2 - 1; //找到最后一个分支
+	while(curpos >= 0)
+	{
+		HeapShiftDown(php, curpos, n);
+		curpos--;
+	}
 }
 
 void HeapInsert(MinHeap *php, HeapElemType x)
@@ -45,9 +67,43 @@ void HeapInsert(MinHeap *php, HeapElemType x)
 
 void HeapErase(MinHeap *php)
 {
+	assert(php != NULL);
+	if(php->size == 0)
+	{
+		printf("堆已空，不能删除堆顶元素.\n");
+		return;
+	}
 
+	php->size--;
+	php->base[0] = php->base[php->size];
+	HeapShiftDown(php, 0, php->size);
 }
 
+bool HeapEmpty(MinHeap *php)
+{
+	assert(php != NULL);
+	return php->size == 0;
+}
+
+HeapElemType HeapTop(MinHeap *php)
+{
+	assert(php!=NULL && php->size>0);
+	return php->base[0];
+}
+
+void HeapSort(MinHeap *php, int ar[], int n)
+{
+	HeapCreate(php, ar, n);
+	while(php->size > 0)
+	{
+		php->size--;
+		Swap(&php->base[php->size], &php->base[0]);
+		HeapShiftDown(php, 0, php->size);
+	}
+
+	for(int i=0; i<n; ++i)
+		ar[i] = php->base[i];
+}
 
 void HeapShow(MinHeap *php)
 {
@@ -74,9 +130,23 @@ void HeapShiftUp(MinHeap *php, int start) //i=parent j=child
 	}
 }
 
-void HeapShiftDown(MinHeap *php, int start)
+void HeapShiftDown(MinHeap *php, int start, int n)
 {
-
+	int i = start;
+	int j = 2*i+1;
+	while(j < n)
+	{
+		if(j+1<n && php->base[j]>php->base[j+1]) //寻求左右子树中的最小值
+			j++;
+		if(php->base[i] > php->base[j])
+		{
+			Swap(&php->base[i], &php->base[j]);
+			i = j;
+			j = 2*i+1;
+		}
+		else 
+			break;
+	}
 }
 
 #endif /* _HEAP_H_ */
